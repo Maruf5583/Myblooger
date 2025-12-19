8c63cf1c53acab4e98400822b9116f80585e1d47
Md Maruf Hasan <marufhasanash@gmail.com>
2025-12-19 23:33:48 +0600

    uplad new code

--- MyBlog/Controllers/MathController.cs
+++ MyBlog/Controllers/MathController.cs
@@ -30,5 +30,33 @@ namespace MyBlog.Controllers
        {
             return firstNumber + secondNumber;
         }
+
+
+        public IActionResult Substraction()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Substraction(float firstNumber, float secondNumber)
+        {
+            if (firstNumber > 0 && secondNumber > 0)
+            {
+
+                float result = sub(firstNumber, secondNumber);
+
+                ViewBag.Result = result;
+            }
+            else
+            {
+                return BadRequest("number will be getter then 0");
+            }
+
+            return View();
+        }
+
+        private float sub(float firstNumber, float secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
     }
 }
